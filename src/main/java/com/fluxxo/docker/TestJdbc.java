b66d594a7420bfe4d9d2e9ad082b256f8e69897b
Jan-Hendrik Lendholt <jan.lendholt@innogames.com>
2017-02-16 09:15:54 +0100

    initial commit

---
+++ src/main/java/com/fluxxo/docker/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.fluxxo.docker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by jan.lendholt on 15.02.17.
+ */
+public class TestJdbc {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
+        Class.forName("com.mysql.jdbc.Driver");
+        // Setup the connection with the DB
+        try {
+            Connection connect = DriverManager
+                    .getConnection("jdbc:mysql://mysql/test?"
+                            + "user=test&password=test");
+            System.out.println("Connection closed? "+connect.isClosed());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Thread.sleep(3600000);
+    }
+}
